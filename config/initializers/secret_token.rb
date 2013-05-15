3b9b0be0c8f3f77c55dbaec785e2fbe8e237e610
Michael Darling <mdarling@ece.unm.edu>
2013-05-14 19:12:52 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matcher::Application.config.secret_token = '370890351d0a442c89e175d2a7455ce641d7edfd2cebb1c62e33c96c81bcd23a789e9cdd5890301ef04ebaab4313c4b09908c18982753d45083c6647293d4eab'

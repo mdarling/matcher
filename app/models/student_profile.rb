cf76c19d33b3061e5f65c48aee89c7a9822cdd3c
Michael Darling <mdarling@ece.unm.edu>
2013-05-21 20:05:14 -0600

    Finished Student profile GUI

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,3 +1,4 @@
 class StudentProfile < ActiveRecord::Base
-  attr_accessible :academicLevel, :email, :expectedGraduation, :firstName, :interests, :lastName, :major, :minor, :phoneNumber, :unmID
+  attr_accessible :academicLevel, :email, :expectedGraduation, :firstName, 
+	:interests, :lastName, :major, :minor, :phoneNumber, :unmID
 end
---
+++ app/models/student_profile.rb~
@@ -0,0 +1,3 @@
+class StudentProfile < ActiveRecord::Base
+  attr_accessible :academicLevel, :email, :expectedGraduation, :firstName, :interests, :lastName, :major, :minor, :phoneNumber, :unmID
+end

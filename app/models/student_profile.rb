479e53f0bdd82035268fc8c5e37f94321251ff32
Michael Darling <mdarling@ece.unm.edu>
2013-05-23 23:48:56 -0600

    final student survey view

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,4 +1,9 @@
 class StudentProfile < ActiveRecord::Base
   attr_accessible :academicLevel, :email, :expectedGraduation, :firstName, 
 	:interests, :lastName, :major, :minor, :phoneNumber, :unmID
+
+	validates :academicLevel, :email, :expectedGraduation, :firstName, 
+	:interests, :lastName, :major, :minor, :phoneNumber, :unmID, :presence => true
+	
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end

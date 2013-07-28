a19e8585a4b8c357e264a83b337a4090878cb26f
Michael Darling <mdarling@ece.unm.edu>
2013-07-28 15:11:09 -0600

    Positions submitted to db correctly

--- app/models/paid_undergrad_position.rb
+++ app/models/paid_undergrad_position.rb
@@ -2,7 +2,7 @@ class PaidUndergradPosition < ActiveRecord::Base
   attr_accessible :academicLevel, :deliverables, :desiredClasses, 
   :desiredSkills, :jobDescription, :minimumGPA, :positionTitle, 
   :postingNumber, :recordBegin, :recordEnd, :unmJobs, :weeklyTimeCommitment, 
-  :workPeriod
+  :workPeriod, :project_survey_id
   
   belongs_to :project_survey
 end
--- app/models/project_survey.rb
+++ app/models/project_survey.rb
@@ -1,11 +1,18 @@
 class ProjectSurvey < ActiveRecord::Base
   attr_accessible :department, :email, :phone, :projectDescription, 
   :projectTitle, :researchArea, :researcher, :unpaid_grads_needed, 
-  :paid_grads_needed, :unpaid_undergrads_needed, :paid_undergrads_needed
+  :paid_grads_needed, :unpaid_undergrads_needed, :paid_undergrads_needed,
+  :unpaid_undergrad_positions_attributes, :paid_undergrad_positions_attributes
 	
 	has_many :unpaid_undergrad_positions
 	has_many :paid_undergrad_positions
 	has_many :unpaid_grad_positions
 	has_many :paid_grad_positions
-
+	
+	accepts_nested_attributes_for :unpaid_undergrad_positions, 
+																:paid_undergrad_positions,
+																:unpaid_grad_positions,
+																:paid_grad_positions,
+																:allow_destroy => true
+															
 end

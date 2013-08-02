b53c57e08eba87cb54f015ed554c3867851ee418
Michael Darling <mdarling@ece.unm.edu>
2013-08-02 14:28:53 -0600

    Removed bad migration

--- db/migrate/20130802195753_fix_column_name.rb
+++
@@ -1,9 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def up
-  rename_column :table_name, :old_column, :new_column
-  rename_column :student_profiles, :department, :string
-  end
-
-  def down
-  end
-end
